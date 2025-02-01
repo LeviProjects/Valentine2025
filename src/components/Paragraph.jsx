602d49ef42a5f26c3436903c781772645a44cc67
RafaelR <rafrosr@gmail.com>
2025-02-01 13:55:04 -0800

    FINAL CHANGES DONEE

--- src/components/Paragraph.jsx
+++ src/components/Paragraph.jsx
@@ -1,16 +1,39 @@
 import React from "react";
+import "../styles/Paragraph.css";
 
 const Paragraph = ({ text }) => {
   return (
     <div className="modal-content">
-      <p>
+      <p className="loveParagraph">
         Hello my beautiful love. This is my official request to have the
         privilege of being your Valentine. I cannot believe it has been another
         year, another year I have gotten to spend with you. I am so grateful for
         all the time we have spent together and I am so excited for all the time
-        we have left to spend together. :)
+        we are yet to spend together. :)
       </p>
-      <p></p>
+      <p className="loveParagraph">
+        I love you so much my beautiful Levs. Mi amor. You are everything and
+        anything that anyone could ever ask for. You are my best friend, and I
+        am so extremely lucky to have you in my life, and even luckier to have
+        the privilege of being able to call myself yours. My heart belongs to
+        you and you alone, no one else can have it.
+      </p>
+      <p className="loveParagraph">
+        {" "}
+        And now, another whole year has passed,and I am so grateful that we have
+        made it this far, and even find comfort in that despite our hardship of
+        not being able to see each other as much as we'd like to. I do truly
+        believe that we will make it, and eventually live the life that we want
+        to live together. I can't help but always feel like we were made for
+        each other, and have been drawn to each other despite that hardships
+        that we've faced.{" "}
+      </p>
+      <p className="loveParagraph">
+        I love you so much my beautiful girl, I am so proud of the person that
+        you are. You are an incredibly beautiful person, your soul shining
+        brighter than everything around it, like the flowers down below. I love
+        you so so much my beautiful Baby 💖{" "}
+      </p>
     </div>
   );
 };
